ecd000a1361cb4b1f5af79c1c2b6915c47f7a28f
radheshyam012 <112542492+radheshyam012@users.noreply.github.com>
2024-04-01 12:51:42 +0530

    Create model & schema

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,10 @@ const app = express();
 const port = 3000;
 
 connectToMongo();
-app.get("/", (req, res) => {
-  res.send("Hello Radhe!");
-});
+//Avaible routes
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes",require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
